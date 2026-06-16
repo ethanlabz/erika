3458ea58239c5a22c2badbc655f4bc8768120e80
dorukaysor <dorukaysor@gmail.com>
2026-06-16 21:40:11 +0530

    ..

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,4 +1,4 @@
-export const appName = 'EthanLabz S5';
+export const appName = 'ErikLabs';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
@@ -6,6 +6,6 @@ export const docsContentRoute = '/llms.mdx/docs';
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
   user: 'ethanlabz',
-  repo: 'sem5',
+  repo: 'erika',
   branch: 'main',
 };
